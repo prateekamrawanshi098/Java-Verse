f7352984944f26672a4ba2a09275b2971f782e5d
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-09 13:32:19 +0530

    print() and println()

---
+++ 04-println-and-print-method/Demo.java
@@ -0,0 +1,38 @@
+class Demo {
+public static void main(String[] args) {
+    System.out.println("Hello Prateek");
+    System.out.println();
+    System.out.println("How are u today!");
+    System.out.print("hello ");
+    System.out.println("prateek\n");
+    System.out.print("how r u today!\n");
+    System.out.println("I\tam\tprogramming");
+}
+}
+
+/*
+pirntln() 
+=======================================
+performs two activity 
+    1.print the output 
+    2.put the cursor into next line
+
+println() without arguement just brings the cursor to next line
+
+
+print() 
+========================================
+ only prints the output cannot run without arguement
+ \n can be used with print() to give newlinw which is also called new line character can also be used with println()
+
+
+
+
+
+ /t
+ ======
+ used to make space between cloumns
+ it always brings the cursor to the next column tab (each 'column tab' contain 8 columns)
+*/
+
+
