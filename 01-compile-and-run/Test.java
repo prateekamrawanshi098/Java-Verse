c6b90dcac78cad44c7506fdd829a88a9006d9528
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-09 11:22:55 +0530

    compile and run

---
+++ 01-compile-and-run/Test.java
@@ -0,0 +1,28 @@
+class Test{
+    public static void main(String[] args){
+        System.out.println("Hello User");
+    }
+}
+
+/*  How to compile A java Code?
+ =============================
+ javac <File Name>
+
+ ex: javac Test.java
+ Whenever we compile a java code , the compiler takes 3 action :
+
+ 1.Syntax checking,
+ 2.Compiling the code,
+ 3.create bytecode file :
+
+         a.They have the extenstion in .class          b.They have same name of class defines by programmer
+         c. No. of bytecode file is always equal to no. of class defines by programmers
+*/
+
+
+/*
+To run java code 
+Syntax:
+======
+java Main-class
+*/
