f8f5ad57cfcca5ee2953825271c33900b7568d8c
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-09 12:24:32 +0530

    File name and class name myth

---
+++ 02-classname-myth/Demo.java
@@ -0,0 +1,11 @@
+class Bhopal{
+    public static void main(String[] args) {
+        System.out.println("Bhopal is the city of lakes");
+    }
+}
+
+/*
+we can write file name and class name different unless class is public 
+
+although it is recommended to keep the filename and class name same so that we do not need to remember both the names for compiling and running
+*/
