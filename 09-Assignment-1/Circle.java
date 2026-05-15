fe1480ddeab936b3ae8f1a0ebbdf166c17f1e2e0
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-15 12:36:38 +0530

    Assignment-1

---
+++ 09-Assignment-1/Circle.java
@@ -0,0 +1,13 @@
+
+/*WAP to calculate area and circumference of a circle .Assume that 
+be an integer and intialize it with any value of your choice
+*/
+class Circle {
+    public static void main(String[] args) {
+        int rad = 5;
+        double cirm =2* 3.14 * rad;
+        double Area = 3.14*rad * rad;
+        System.out.println("Circumference of circle is " + cirm);
+        System.out.println("Area of circle is "+Area);
+    }
+}
