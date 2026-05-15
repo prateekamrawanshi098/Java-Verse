df30310edc71038ff8be760db43db96f891f0f17
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-15 12:56:17 +0530

    The Math class

---
+++ 10-Math-class/Circle.java
@@ -0,0 +1,8 @@
+
+ class Circle {
+     public static void main(String[] args) {
+         int rad = 5;
+         System.out.println("Circumference of circle is " + 2 * Math.PI * rad);
+        System.out.println("Area of circle is "+Math.PI*Math.pow(rad, 2));
+    }
+}
