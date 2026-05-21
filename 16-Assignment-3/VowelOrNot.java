2372aca73cdec42e671e3eb5a4ab53ecdfa753f0
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-21 14:02:49 +0530

    VowelOrNot improved

--- 16-Assignment-3/VowelOrNot.java
+++ 16-Assignment-3/VowelOrNot.java
@@ -3,8 +3,8 @@
 public class VowelOrNot {
 
     public static void main(String[] args) {
-        char c = args[0].charAt(0);
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+        char c = args[0].toUpperCase().charAt(0);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ) {
             System.out.println("character is vowel");
         }
         else
