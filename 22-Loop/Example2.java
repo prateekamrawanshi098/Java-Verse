76f6ed926a11f096318c24719324f13de1a9301c
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-06-18 11:37:02 +0530

    Loops

---
+++ 22-Loop/Example2.java
@@ -0,0 +1,12 @@
+class Example2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i--;
+        }
+        System.out.println("Good Day!");
+    }
+}
+
+//it will print from 1 to minimum value of int
---
+++ 22-Loop/Example3.java
@@ -0,0 +1,12 @@
+class Example3 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i >= 10) {
+            System.out.println(1);
+            i++;
+        }
+        System.out.println("Good Day");
+    }
+}
+//since condition is false loop while not start and just print 
+//Good Day
